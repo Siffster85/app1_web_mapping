e3dfc93dc331af01b817e8505738201f3050597b
Siffster85 <ylsaxon@googlemail.com>
2024-05-31 11:50:13 +0100

    Added feature group for markers

--- map1.py
+++ map1.py
@@ -1,3 +1,10 @@
 import folium
-map = folium.Map(location=[53.48, -2.24], zoom_start=10)
+map = folium.Map(location=[53.48, -2.24], zoom_start=12)
+
+fg = folium.FeatureGroup(name="Test Map")
+
+fg.add_child(folium.Marker(location=[53.46, -2.22], popup="Test Marker", icon=folium.Icon(color='blue')))
+
+map.add_child(fg)
+
 map.save("Map1.html")
