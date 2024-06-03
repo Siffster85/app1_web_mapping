bbe8e995e1f264b75022380f2dba7a5cc41d1b12
Siffster85 <ylsaxon@googlemail.com>
2024-06-03 12:23:41 +0100

    Added population colour coding

--- map1.py
+++ map1.py
@@ -23,7 +23,8 @@ fg = folium.FeatureGroup(name="Test Map")
 for lt, ln, nm, el in zip(lat, lon, name, elev):
     fg.add_child(folium.CircleMarker(location=[lt, ln], radius=7, popup=nm + " volcano elevation stands at " + str(el) + " metres above sea level.", fill_color=colour_set(el), color='black', fill_opacity=0.8))
 
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read())))
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 100000000 else 'red'}))
 
 map.add_child(fg)
 map.save("Map1.html")
