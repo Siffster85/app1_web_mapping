14aa74cf948764196659cba883002121f081f6f7
Siffster85 <ylsaxon@googlemail.com>
2024-06-03 12:56:10 +0100

    added layer control and readme.

--- map1.py
+++ map1.py
@@ -18,13 +18,16 @@ def colour_set(elevation):
 
 map = folium.Map(location=[40.73, -114.07], zoom_start=6)
 
-fg = folium.FeatureGroup(name="Test Map")
+fgp = folium.FeatureGroup(name="Population")
 
-for lt, ln, nm, el in zip(lat, lon, name, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=7, popup=nm + " volcano elevation stands at " + str(el) + " metres above sea level.", fill_color=colour_set(el), color='black', fill_opacity=0.8))
-
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
 style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 100000000 else 'red'}))
 
-map.add_child(fg)
+fgv = folium.FeatureGroup(name="Volcanoes")
+for lt, ln, nm, el in zip(lat, lon, name, elev):
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=7, popup=nm + " volcano elevation stands at " + str(el) + " metres above sea level.", fill_color=colour_set(el), color='black', fill_opacity=0.8))
+
+map.add_child(fgp)
+map.add_child(fgv)
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
---
+++ readme.md
@@ -0,0 +1,10 @@
+A basic python app create a map with three layers, base, marker and polygon.
+
+Using Folium and Pandas this app creates a map of the world and has added markers to 62 volcanoes in the western half of the USA, the markers are colour coded based on elevation above sea level and have a popup with the name and elevation.
+
+The Polygon layer colour codes the population density of each country, green for under 10m, yellow for up to 100m and red for higher populations.
+
+Layer Control is available with the icon in the top right hand corner.
+
+How to run:
+Open the .html file in a browser of your choice.
