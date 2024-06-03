7bc04ba9f826013a8781087e250c73a239d80648
Siffster85 <ylsaxon@googlemail.com>
2024-06-03 10:26:29 +0100

    Added elevation specific colour coding for the markers and changed them to circles.

--- map1.py
+++ map1.py
@@ -8,12 +8,20 @@ lon = list(data["LON"])
 name = list(data["NAME"])
 elev = list(data["ELEV"])
 
+def colour_set(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 2500:
+        return 'orange'
+    else:
+        return 'red'
+
 map = folium.Map(location=[40.73, -114.07], zoom_start=6)
 
 fg = folium.FeatureGroup(name="Test Map")
 
 for lt, ln, nm, el in zip(lat, lon, name, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=nm + " volcano elevation stands at " + str(el) + " metres", icon=folium.Icon(color='red')))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=7, popup=nm + " volcano elevation stands at " + str(el) + " metres above sea level.", fill_color=colour_set(el), color='black', fill_opacity=0.8))
 
 map.add_child(fg)
 
